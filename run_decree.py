7ff8ec29d432d96dc5c22b509c04a413431e301f
seaskyho <seaskyho@gmail.com>
2025-10-27 00:08:21 +1100

    update

--- run_decree.py
+++ run_decree.py
@@ -53,132 +53,132 @@ result_file = f"resultfinal_cliptext_{timestamp}.txt"
 """
 # # DECREE's Provided Clean and Backdoored Encoders
 """
-# dir_list = [
-#     "output/CLIP_text/clean_encoder",
-#     "output/CLIP_text/gtsrb_backdoored_encoder",
-#     "output/CLIP_text/stl10_backdoored_encoder",
-#     "output/CLIP_text/svhn_backdoored_encoder",
-# ]
-# for dir in dir_list:
-#     dir_file_list = listdir(dir)
-#     encoder_list = [
-#         f for f in dir_file_list if (isfile(join(dir, f)) and (f.find(".pth") != -1))
-#     ]
-#     for encoder in encoder_list:
-#         enc_path = f"{dir}/{encoder}"
-#         id = f'_{dir.split("/")[1]}_{dir.split("/")[2].split("_")[0]}_{encoder}'  # _CLIP_text_gtsrb_model_10_tg24_imagenet.pth
-#         flag = "clean" if "clean" in dir else "backdoor"
+dir_list = [
+    "output/CLIP_text/clean_encoder",
+    "output/CLIP_text/gtsrb_backdoored_encoder",
+    "output/CLIP_text/stl10_backdoored_encoder",
+    "output/CLIP_text/svhn_backdoored_encoder",
+]
+for dir in dir_list:
+    dir_file_list = listdir(dir)
+    encoder_list = [
+        f for f in dir_file_list if (isfile(join(dir, f)) and (f.find(".pth") != -1))
+    ]
+    for encoder in encoder_list:
+        enc_path = f"{dir}/{encoder}"
+        id = f'_{dir.split("/")[1]}_{dir.split("/")[2].split("_")[0]}_{encoder}'  # _CLIP_text_gtsrb_model_10_tg24_imagenet.pth
+        flag = "clean" if "clean" in dir else "backdoor"
 
-#         ### run CLIP_text
-#         run(
-#             gpu,
-#             flag,
-#             "rand",
-#             enc_path=enc_path,
-#             arch=arch,
-#             result_file=result_file,
-#             encoder_usage_info="CLIP",
-#             lr=0.5,
-#             batch_size=32,
-#             id=id,
-#             timestamp=timestamp,
-#             model_source="decree",
-#         )
+        ### run CLIP_text
+        run(
+            gpu,
+            flag,
+            "rand",
+            enc_path=enc_path,
+            arch=arch,
+            result_file=result_file,
+            encoder_usage_info="CLIP",
+            lr=0.5,
+            batch_size=32,
+            id=id,
+            timestamp=timestamp,
+            model_source="decree",
+        )
 
-# """
-# # Additional Backdoored Encoders from https://huggingface.co/models?other=arxiv:2502.01385
-# """
-# hanxun_backdoor_list = [
-#     "clip_backdoor_rn50_cc3m_badnets",
-#     "clip_backdoor_rn50_cc3m_clean_label",
-#     "clip_backdoor_rn50_cc3m_blend",
-#     "clip_backdoor_rn50_cc3m_sig",
-#     "clip_backdoor_rn50_cc3m_nashville",
-#     "clip_backdoor_rn50_cc3m_wanet",
-#     "clip_backdoor_rn50_cc3m_blto_cifar",
-#     "clip_backdoor_rn50_cc12m_badnets",
-#     "clip_backdoor_rn50_cc12m_clean_label",
-#     "clip_backdoor_rn50_cc12m_blend",
-#     "clip_backdoor_rn50_cc12m_sig",
-#     "clip_backdoor_rn50_cc12m_nashville",
-#     "clip_backdoor_rn50_redcaps_badnets",
-#     "clip_backdoor_rn50_redcaps_clean_label",
-#     "clip_backdoor_rn50_redcaps_blend",
-#     "clip_backdoor_rn50_redcaps_sig",
-#     "clip_backdoor_rn50_redcaps_nashville",
-#     "clip_backdoor_rn50_redcaps_wanet",
-#     # "clip_backdoor_rn50_cc12m_wanet",  # FIXME: this one has error
-#     "clip_backdoor_vit_b16_cc3m_badnets",
-#     "clip_backdoor_vit_b16_cc3m_clean_label",
-#     "clip_backdoor_vit_b16_cc3m_blend",
-#     "clip_backdoor_vit_b16_cc3m_sig",
-#     "clip_backdoor_vit_b16_cc3m_nashville",
-#     "clip_backdoor_vit_b16_cc3m_wanet",
-#     "clip_backdoor_vit_b16_cc3m_blto_cifar",
-# ]
-# prefix = "hf-hub:hanxunh/"
-# for model_name in hanxun_backdoor_list:
-#     run(
-#         gpu,
-#         "backdoor",
-#         "rand",
-#         enc_path=prefix + model_name,
-#         arch=arch,
-#         result_file=result_file,
-#         encoder_usage_info="CLIP",
-#         lr=0.5,
-#         batch_size=32,
-#         id=model_name,
-#         timestamp=timestamp,
-#         model_source="hanxun",
-#     )
+"""
+# Additional Backdoored Encoders from https://huggingface.co/models?other=arxiv:2502.01385
+"""
+hanxun_backdoor_list = [
+    "clip_backdoor_rn50_cc3m_badnets",
+    "clip_backdoor_rn50_cc3m_clean_label",
+    "clip_backdoor_rn50_cc3m_blend",
+    "clip_backdoor_rn50_cc3m_sig",
+    "clip_backdoor_rn50_cc3m_nashville",
+    "clip_backdoor_rn50_cc3m_wanet",
+    "clip_backdoor_rn50_cc3m_blto_cifar",
+    "clip_backdoor_rn50_cc12m_badnets",
+    "clip_backdoor_rn50_cc12m_clean_label",
+    "clip_backdoor_rn50_cc12m_blend",
+    "clip_backdoor_rn50_cc12m_sig",
+    "clip_backdoor_rn50_cc12m_nashville",
+    "clip_backdoor_rn50_redcaps_badnets",
+    "clip_backdoor_rn50_redcaps_clean_label",
+    "clip_backdoor_rn50_redcaps_blend",
+    "clip_backdoor_rn50_redcaps_sig",
+    "clip_backdoor_rn50_redcaps_nashville",
+    "clip_backdoor_rn50_redcaps_wanet",
+    # "clip_backdoor_rn50_cc12m_wanet",  # FIXME: this one has error
+    "clip_backdoor_vit_b16_cc3m_badnets",
+    "clip_backdoor_vit_b16_cc3m_clean_label",
+    "clip_backdoor_vit_b16_cc3m_blend",
+    "clip_backdoor_vit_b16_cc3m_sig",
+    "clip_backdoor_vit_b16_cc3m_nashville",
+    "clip_backdoor_vit_b16_cc3m_wanet",
+    "clip_backdoor_vit_b16_cc3m_blto_cifar",
+]
+prefix = "hf-hub:hanxunh/"
+for model_name in hanxun_backdoor_list:
+    run(
+        gpu,
+        "backdoor",
+        "rand",
+        enc_path=prefix + model_name,
+        arch=arch,
+        result_file=result_file,
+        encoder_usage_info="CLIP",
+        lr=0.5,
+        batch_size=32,
+        id=model_name,
+        timestamp=timestamp,
+        model_source="hanxun",
+    )
 
 """
 # Additional: Open-clip https://github.com/mlfoundations/open_clip?tab=readme-ov-file
 """
 openclip_clean_list = [
-    # ("RN50", "openai"),
-    # ("RN50", "yfcc15m"),
-    # ("RN50", "cc12m"),
-    # ("RN101", "openai"),
-    # ("RN101", "yfcc15m"),
-    # ("ViT-B-16", "openai"),
-    # ("ViT-B-16", "laion400m_e31"),
-    # ("ViT-B-16", "laion400m_e32"),
-    # ("ViT-B-16", "laion2b_s34b_b88k"),
-    # ("ViT-B-16", "datacomp_xl_s13b_b90k"),
-    # ("ViT-B-16", "datacomp_l_s1b_b8k"),
-    # ("ViT-B-16", "commonpool_l_clip_s1b_b8k"),
-    # ("ViT-B-16", "commonpool_l_laion_s1b_b8k"),
-    # ("ViT-B-16", "commonpool_l_image_s1b_b8k"),
-    # ("ViT-B-16", "commonpool_l_text_s1b_b8k"),
-    # ("ViT-B-16", "commonpool_l_basic_s1b_b8k"),
-    # ("ViT-B-16", "commonpool_l_s1b_b8k"),
-    # ("ViT-B-16", "dfn2b"),
-    # ("ViT-B-16", "metaclip_400m"),
-    # ("ViT-B-16", "metaclip_fullcc"),
-    # ("ViT-B-32", "openai"),
-    # ("ViT-B-32", "laion400m_e31"),
-    # ("ViT-B-32", "laion400m_e32"),
-    # ("ViT-B-32", "laion2b_e16"),
-    # ("ViT-B-32", "laion2b_s34b_b79k"),
-    # ("ViT-B-32", "datacomp_xl_s13b_b90k"),
-    # ("ViT-B-32", "datacomp_m_s128m_b4k"),
-    # ("ViT-B-32", "commonpool_m_clip_s128m_b4k"),
-    # ("ViT-B-32", "commonpool_m_laion_s128m_b4k"),
-    # ("ViT-B-32", "commonpool_m_image_s128m_b4k"),
-    # ("ViT-B-32", "commonpool_m_text_s128m_b4k"),
-    # ("ViT-B-32", "commonpool_m_basic_s128m_b4k"),
-    # ("ViT-B-32", "commonpool_m_s128m_b4k"),
-    # ("ViT-B-32", "datacomp_s_s13m_b4k"),
-    # ("ViT-B-32", "commonpool_s_clip_s13m_b4k"),
-    # ("ViT-B-32", "commonpool_s_laion_s13m_b4k"),
-    # ("ViT-B-32", "commonpool_s_image_s13m_b4k"),
-    # ("ViT-B-32", "commonpool_s_text_s13m_b4k"),
-    # ("ViT-B-32", "commonpool_s_basic_s13m_b4k"),
-    # ("ViT-B-32", "commonpool_s_s13m_b4k"),
-    # ("ViT-B-32", "metaclip_400m"),
-    # ("ViT-B-32", "metaclip_fullcc"),
+    ("RN50", "openai"),
+    ("RN50", "yfcc15m"),
+    ("RN50", "cc12m"),
+    ("RN101", "openai"),
+    ("RN101", "yfcc15m"),
+    ("ViT-B-16", "openai"),
+    ("ViT-B-16", "laion400m_e31"),
+    ("ViT-B-16", "laion400m_e32"),
+    ("ViT-B-16", "laion2b_s34b_b88k"),
+    ("ViT-B-16", "datacomp_xl_s13b_b90k"),
+    ("ViT-B-16", "datacomp_l_s1b_b8k"),
+    ("ViT-B-16", "commonpool_l_clip_s1b_b8k"),
+    ("ViT-B-16", "commonpool_l_laion_s1b_b8k"),
+    ("ViT-B-16", "commonpool_l_image_s1b_b8k"),
+    ("ViT-B-16", "commonpool_l_text_s1b_b8k"),
+    ("ViT-B-16", "commonpool_l_basic_s1b_b8k"),
+    ("ViT-B-16", "commonpool_l_s1b_b8k"),
+    ("ViT-B-16", "dfn2b"),
+    ("ViT-B-16", "metaclip_400m"),
+    ("ViT-B-16", "metaclip_fullcc"),
+    ("ViT-B-32", "openai"),
+    ("ViT-B-32", "laion400m_e31"),
+    ("ViT-B-32", "laion400m_e32"),
+    ("ViT-B-32", "laion2b_e16"),
+    ("ViT-B-32", "laion2b_s34b_b79k"),
+    ("ViT-B-32", "datacomp_xl_s13b_b90k"),
+    ("ViT-B-32", "datacomp_m_s128m_b4k"),
+    ("ViT-B-32", "commonpool_m_clip_s128m_b4k"),
+    ("ViT-B-32", "commonpool_m_laion_s128m_b4k"),
+    ("ViT-B-32", "commonpool_m_image_s128m_b4k"),
+    ("ViT-B-32", "commonpool_m_text_s128m_b4k"),
+    ("ViT-B-32", "commonpool_m_basic_s128m_b4k"),
+    ("ViT-B-32", "commonpool_m_s128m_b4k"),
+    ("ViT-B-32", "datacomp_s_s13m_b4k"),
+    ("ViT-B-32", "commonpool_s_clip_s13m_b4k"),
+    ("ViT-B-32", "commonpool_s_laion_s13m_b4k"),
+    ("ViT-B-32", "commonpool_s_image_s13m_b4k"),
+    ("ViT-B-32", "commonpool_s_text_s13m_b4k"),
+    ("ViT-B-32", "commonpool_s_basic_s13m_b4k"),
+    ("ViT-B-32", "commonpool_s_s13m_b4k"),
+    ("ViT-B-32", "metaclip_400m"),
+    ("ViT-B-32", "metaclip_fullcc"),
     ("ViT-L-14", "openai"),
     ("ViT-L-14", "laion400m_e31"),
     ("ViT-L-14", "laion400m_e32"),
