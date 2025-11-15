5046f98e503fd2f01c5154383f1e7ea8eea58c71
seaskyho <seaskyho@gmail.com>
2025-11-15 11:13:38 +1100

    update

--- read_results.py
+++ read_results.py
@@ -6,29 +6,30 @@ output_file = "z_results.txt"
 output_acc_asr_file_handle = open(output_file, "w", encoding="utf-8")
 
 metric_pattern = r".*Clean Acc Top-1:\s*([\d.]+)\s*ASR Top-1:\s*([\d.]+).*"
-id_pattern = r"(\S+)$"
+id_pattern = r">>> Evaluate encoder.*(\S+)$"
 
 metric_store = []
 id_store = []
 with open(file_path, "r") as f:
-    file_content = f.read()
+    for line in f:
 
-    # metric
-    metric_pattern_match = re.search(metric_pattern, file_content)
-    if metric_pattern_match:
-        acc = float(metric_pattern_match.group(1)) * 100
-        asr = float(metric_pattern_match.group(2)) * 100
+        # metric
+        metric_pattern_match = re.search(metric_pattern, line)
+        if metric_pattern_match:
+            acc = float(metric_pattern_match.group(1)) * 100
+            asr = float(metric_pattern_match.group(2)) * 100
 
-        metric_store.append((acc, asr))
+            metric_store.append((acc, asr))
 
-    # id
-    id_pattern_match = re.search(id_pattern, file_content)
-    if id_pattern_match:
-        id = id_pattern_match.group(1)
-        id_store.append(id)
+        # id
+        id_pattern_match = re.search(id_pattern, line)
+        if id_pattern_match:
+            id = id_pattern_match.group(1)
+            id_store.append(id)
 
 output_lines = [
     f"{id}\t{metric[0]}\t{metric[1]}\n" for id, metric in zip(id_store, metric_store)
 ]
+
 for line in output_lines:
     output_acc_asr_file_handle.write(line)
