575462b2d3893c2cce53ab089c451304f2665fd5
seaskyho <seaskyho@gmail.com>
2025-11-15 11:24:28 +1100

    update

--- read_results.py
+++ read_results.py
@@ -1,7 +1,7 @@
 import re
 
 
-file_path = "slurm-18612723-[PART_3].out"
+file_path = "slurm-18647897-[aug].out"
 output_file = "z_results.txt"
 output_acc_asr_file_handle = open(output_file, "w", encoding="utf-8")
 
