59707434e1d95262f79a08cfefbb7305fbc3ab30
seaskyho <seaskyho@gmail.com>
2025-12-05 15:36:00 +1100

    update

--- mytest.py
+++ mytest.py
@@ -15,6 +15,7 @@ sig_trigger = torch.load("trigger/SIG_noise.pt")
 wanet_trigger = torch.load("trigger/WaNet_grid_temps.pt")
 wanet_trigger = kornia_aug.Resize(size=(224, 224))(wanet_trigger.permute(0, 3, 1, 2))
 wanet_trigger = wanet_trigger.permute(0, 2, 3, 1)
+wanet_trigger = wanet_trigger.repeat(1, 1, 1, 1)
 print(wanet_trigger.shape)
 
 
